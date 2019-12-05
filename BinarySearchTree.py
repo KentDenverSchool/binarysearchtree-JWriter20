56689c41a8a831d865d02df5dded7724cbc8fa48
Jake Writer <jakewriter@Jakes-MacBook-Pro.local>
2019-12-05 16:23:08 -0700

    added testing file and worked on size, minor cleanups

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,40 +1,50 @@
 import TreeNode
+
+#add header and method headers
 class BinarySearchTree:
     def __init__(self):
         self.root = None
-        self.s = 0
 
     def size(self):
-        return self.s
+        return self.size(self.root)
+
+    def size(self, x):
+        s = 0
+        if x.getRight() is not None:
+            s += 1
+            self.size(x.getRight())
+        if x.getLeft() is not None:
+            s += 1
+            self.size(x.getLeft())
+        if x.getLeft() is None and x.getRight() is None:
+            return s
+
 
     def isEmpty(self):
-        if self.s is 0:
+        if self.size(self.root) is 0:
             return True
         else:
             return False
+
     def put(self, key, value):
         self.root = self.put(self.root, key, value)
 
     def put(self, n, key, value):
         if n is None:
             self.root = TreeNode(key, value)
-            self.s += 1
             return self.root
         if n.getKey() < key:
             if n.getLeft() is None:
-                newNode = TreeNode(key,value)
+                newNode = TreeNode(key, value)
                 n.setLeft(newNode)
-                self.s += 1
             else:
                 n.setLeft(self.put(n.getLeft(), key, value))
         if n.getKey() > key:
             if n.getRight() is None:
                 newNode = TreeNode(key, value)
                 n.setRight(newNode)
-                self.s += 1
             else:
                 n.setRight(self.put(n.getRight(), key, value))
-
 
     def get(self, key):
         return self.get(self.root, key);
@@ -56,15 +66,34 @@ class BinarySearchTree:
             return n
 
     def contains(self, key):
-        if(self.root.get(key) is not None):
+        if self.root.get(key) is not None:
             return True
         else:
             return False
 
-
     def remove(self, key):
+        v = self.get(key)
+        root = self.remove(self.root, key)
+        return v
 
     def remove(self, n, key):
+        if n is None:
+            return None
+        if n.getKey < key:
+            n.setLeft(self.remove(n.getLeft(), key))
+        elif n.getKey > key:
+            n.setRight(self.remove(n.getRight(), key));
+        else:
+            if n.getRight() is None:
+                return n.getLeft()
+            if n.getLeft() is None:
+                return n.getRight()
+            mini = self.min(n.getRight())
+            mini.setLeft(n.getLeft())
+            n = n.getRight()
+
+        n.setSize(self.size(n.getRight()) + self.size(n.getLeft()) + 1)
+        return n
 
     def min(self):
         return self.min(self.root).getKey()
@@ -77,7 +106,6 @@ class BinarySearchTree:
         else:
             return n.getLeft()
 
-
     def max(self):
         return self.max(self.root).getKey()
 
@@ -89,13 +117,12 @@ class BinarySearchTree:
         else:
             return n.getRight()
 
-
     def __repr__(self):
         temp = repr(self.root)
         temp = temp.substring(0, temp.length() - 2)
         return "{" + temp + "}"
 
     def __repr__(self, n):
-        if n == None:
+        if n is None:
             return ""
         return repr(n.getLeft()) + n.getKey() + "=" + n.getValue() + ", " + repr(n.getRight())
---
+++ BinarySearchTreeTesting.py
@@ -0,0 +1,20 @@
+#add quick header
+if __name__ == '__main__':
+    print("yee")
+    #testing size()
+
+    #testing isEmpty()
+
+    #testing put()
+
+    #testing get()
+
+    #testing contains()
+
+    #testing remove()
+
+    #testing min()
+
+    #testing max()
+
+    #testing toSting()/repr()
